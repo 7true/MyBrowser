b9fd0589430856dd36369008f9b694068dce3022
alx <37true@gmail.com>
2018-12-23 19:25:01 +0700

    Initial commit

---
+++ app/src/main/java/tk/alltrue/mybrowser/MainActivity.java
@@ -0,0 +1,20 @@
+package tk.alltrue.mybrowser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = findViewById(R.id.webView);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("http://alltrue.tk");
+    }
+}
